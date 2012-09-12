1de17ecb660d009a2fde9d0d4246f7cf8e0fe4f3
Will Ross <paxswill@gmail.com>
2012-09-12 19:14:06 -0400

    uthenticate to the twitter API

--- cs495_twitter.py
+++ cs495_twitter.py
@@ -2,17 +2,27 @@
 
 import twitter
 
-def post(twitter_handle, tweet):
+def post(api_handle, tweet):
     pass
 
-def last_100(twitter_handle, username):
+def last_100(api_handle, username):
     pass
 
 def list_hashtags(tweets):
     pass
 
 def main():
-    pass
+    # Read in the secrets
+    secrets = dict()
+    for key in ('consumer_key',
+                'consumer_secret',
+                'access_token_key',
+                'access_token_secret'):
+        with open(key, 'r') as token:
+            secrets[key] = token.readline()
+        # Trim the newline off
+        secrets[key] = secrets[key][:-1]
+    api = twitter.Api(**secrets)
 
 if __name__ == '__main__':
     main()
