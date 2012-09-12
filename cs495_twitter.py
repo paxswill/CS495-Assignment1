f97a52ff9f81ca3514a063bc19c21a9656028bf1
Will Ross <paxswill@gmail.com>
2012-09-12 18:44:56 -0400

    Stub out the functions

--- cs495_twitter.py
+++ cs495_twitter.py
@@ -1,3 +1,16 @@
 import twitter
 
+def post(twitter_handle, tweet):
+    pass
 
+def last_100(twitter_handle, username):
+    pass
+
+def list_hashtags(tweets):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
