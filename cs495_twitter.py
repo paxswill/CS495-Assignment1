f6872caa44dca8a0a0cf5eaacc7ba3751173cef5
Will Ross <paxswill@gmail.com>
2012-09-12 19:50:37 -0400

    Have the runner function prompt for data

--- cs495_twitter.py
+++ cs495_twitter.py
@@ -30,6 +30,23 @@ def main():
         # Trim the newline off
         secrets[key] = secrets[key][:-1]
     api = twitter.Api(**secrets)
+    # Post a tweet
+    tweet = ""
+    while len(tweet) == 0 or len(tweet) > 140:
+        tweet = raw_input("Enter a tweet: ")
+        if len(tweet) > 140:
+            print "Your tweet is too long (over 140 characters)."
+    post(api, tweet)
+    # Get the last 100 for someone
+    userid = raw_input("Enter a username to lookup: ")
+    tweets = last_100(api, userid)
+    for tweet in tweets:
+        print tweet.user.name + ": " + tweet.text
+    # List hashtags
+    print "Hashtags: "
+    for hashtag in list_hashtags(tweets):
+        print hashtag
+
 
 if __name__ == '__main__':
     main()
